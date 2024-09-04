32b8e8d4cdac275a3ec623bdfbcdd6db510ae345
VladyslavKovall <fff523179@gmail.com>
2024-09-04 14:10:43 +0300

    Laba

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Я Влад. Тут є дві крапки. Ось остання.Привіт";
+            int lastDotIndex = s.LastIndexOf('.');
+            int countAfterLastDot = s.Length - lastDotIndex - 1;
+            Console.WriteLine("Кількість символів після останньої точки: " + countAfterLastDot);
+
+
+        }
+    }
+}
